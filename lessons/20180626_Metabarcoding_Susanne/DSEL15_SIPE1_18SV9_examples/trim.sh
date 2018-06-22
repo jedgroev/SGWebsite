8298e3fa74b44efd5c11e7ee92357d8afccbd187
Marc Galland <m.galland@uva.nl>
2018-06-22 14:50:31 +0100

    fixed error in -o and -p

--- lessons/20180626_Metabarcoding_Susanne/DSEL15_SIPE1_18SV9_examples/trim.sh
+++ lessons/20180626_Metabarcoding_Susanne/DSEL15_SIPE1_18SV9_examples/trim.sh
@@ -9,13 +9,21 @@
 for=(./*R1_001.fastq)
 rev=(./*R2_001.fastq)
 
+#for item in ${for[*]};do
+#	echo $item
+#done
+
+
 
 for ((i=0;i<${#for[*]};i++)) # iterates over the forward reads array
 do
 	# get short sample names
 	fullname=$(basename -- ${for[i]})
-	shortname="${fullname%_S[0-9+]*}"
-	echo "processing" $shortname
+	sample="${fullname%_S[0-9+]*}"
+	echo "processing" $sample
+	trimmed_for="$sample _R1.trim.fastq"
+	trimmed_rev="$sample _R2.trim.fastq"
+	echo $trimmed_for
 	# cutadapt
-	cutadapt -a TTGTACACACCGCCC...GTAGGTGAACCTGCRGAAGG -A CCTTCYGCAGGTTCACCTAC...GGGCGGTGTGTACAA --match-read-wildcards --discard-untrimmed --pair-filter=any --minimum-length 100 -o ./trimmed/$shortname_R1.trim.fastq -p ./trimmed/$shortname_R2.trim.fastq ${for[i]} ${rev[i]} 2>./trimmed/$shortname.report.txt 
+	cutadapt -a TTGTACACACCGCCC...GTAGGTGAACCTGCRGAAGG -A CCTTCYGCAGGTTCACCTAC...GGGCGGTGTGTACAA --match-read-wildcards --discard-untrimmed --pair-filter=any --minimum-length 100 -o $trimmed_for -p $trimmed_rev ${for[i]} ${rev[i]} 
 done
